5d964c96c1ed31df7f7ac93a5fef5e6e7993bcff
Nifemi Daniel <nifemidaniel50@gmail.com>
2022-08-25 12:01:32 -0700

    Chapter 15

---
+++ src/chapter15/Example2.java
@@ -0,0 +1,14 @@
+package chapter15;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Example2 {
+    public static void main(String[] args) {
+        try(PrintWriter writer = new PrintWriter("C:\\Download\\SemicolonClassworks\\src\\chapter15\\test.txt")) {;
+            writer.println("my first line");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
